e9fc4fadeec395ab109f83504836d890d091bf28
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-02 22:55:22 +0200

    service-impl commit

--- src/main/java/com/backend/ireme_ry/_amashuri/repository/DistrictRepository.java
+++ src/main/java/com/backend/ireme_ry/_amashuri/repository/DistrictRepository.java
@@ -4,4 +4,5 @@ import com.backend.ireme_ry._amashuri.model.District;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DistrictRepository extends JpaRepository <District, Long> {
+    District findDistrictById(Long id);
 }
--- src/main/java/com/backend/ireme_ry/_amashuri/service/DistrictServiceImplementation.java
+++ src/main/java/com/backend/ireme_ry/_amashuri/service/DistrictServiceImplementation.java
@@ -1,34 +1,50 @@
 package com.backend.ireme_ry._amashuri.service;
 
 import com.backend.ireme_ry._amashuri.model.District;
+import com.backend.ireme_ry._amashuri.repository.DistrictRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class DistrictServiceImplementation implements DistrictService{
+
+    private final DistrictRepository districtRepository;
+
+    @Autowired
+    public DistrictServiceImplementation(DistrictRepository districtRepository) {
+        this.districtRepository = districtRepository;
+    }
+
     @Override
     public List<District> getAllDistricts() {
-        return null;
+        return districtRepository.findAll();
     }
 
     @Override
     public District getDistrictById(Long id) {
-        return null;
+        return districtRepository.findDistrictById(id);
     }
 
     @Override
     public District saveDistrict(District district) {
-        return null;
+        return districtRepository.save(district);
     }
 
     @Override
     public District updateDistrict(Long id, District district) {
+        District existDistrict = districtRepository.findDistrictById(id);
+        if(existDistrict != null){
+            existDistrict.setName(district.getName());
+            existDistrict.setProvince(district.getProvince());
+            return districtRepository.save(existDistrict);
+        }
         return null;
     }
 
     @Override
     public void deleteById(Long id) {
-
+        districtRepository.deleteById(id);
     }
 }
