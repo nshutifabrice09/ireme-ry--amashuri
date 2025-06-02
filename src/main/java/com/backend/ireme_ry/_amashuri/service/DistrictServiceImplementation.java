6b2083993b2998a72054086f79ba493d35b1b7cd
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-02 22:48:11 +0200

    service-impl commit

--- src/main/java/com/backend/ireme_ry/_amashuri/service/DistrictServiceImplementation.java
+++ src/main/java/com/backend/ireme_ry/_amashuri/service/DistrictServiceImplementation.java
@@ -1,7 +1,34 @@
 package com.backend.ireme_ry._amashuri.service;
 
+import com.backend.ireme_ry._amashuri.model.District;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class DistrictServiceImplementation implements DistrictService{
+    @Override
+    public List<District> getAllDistricts() {
+        return null;
+    }
+
+    @Override
+    public District getDistrictById(Long id) {
+        return null;
+    }
+
+    @Override
+    public District saveDistrict(District district) {
+        return null;
+    }
+
+    @Override
+    public District updateDistrict(Long id, District district) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+    }
 }
