0ad06399d1ea8dd152109479e32a30564b292d2d
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-02 23:23:53 +0200

    service-impl-methods commit

--- src/main/java/com/backend/ireme_ry/_amashuri/service/UserServiceImplementation.java
+++ src/main/java/com/backend/ireme_ry/_amashuri/service/UserServiceImplementation.java
@@ -1,20 +1,30 @@
 package com.backend.ireme_ry._amashuri.service;
 
 import com.backend.ireme_ry._amashuri.model.User;
+import com.backend.ireme_ry._amashuri.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class UserServiceImplementation implements UserService{
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImplementation(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public List<User> getAllUsers() {
-        return null;
+        return userRepository.findAll();
     }
 
     @Override
     public User getUserById(Long id) {
-        return null;
+        return userRepository.findUserById(id);
     }
 
     @Override
