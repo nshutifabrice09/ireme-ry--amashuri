1d762a9591a27ed113f9a52a22f2b62a9f451ba4
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-02 23:12:31 +0200

    service-impl-methods commit

--- src/main/java/com/backend/ireme_ry/_amashuri/service/SchoolServiceImplementation.java
+++ src/main/java/com/backend/ireme_ry/_amashuri/service/SchoolServiceImplementation.java
@@ -1,7 +1,34 @@
 package com.backend.ireme_ry._amashuri.service;
 
+import com.backend.ireme_ry._amashuri.model.School;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SchoolServiceImplementation implements SchoolService{
+    @Override
+    public List<School> getAllSchools() {
+        return null;
+    }
+
+    @Override
+    public School getSchoolById(Long id) {
+        return null;
+    }
+
+    @Override
+    public School saveSchool(School school, Long districtId, Long headteacherId) {
+        return null;
+    }
+
+    @Override
+    public School updateSchool(Long id, School school) {
+        return null;
+    }
+
+    @Override
+    public void deleteSchoolById(Long id) {
+
+    }
 }
