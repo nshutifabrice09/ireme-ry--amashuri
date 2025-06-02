361fccb72caa3f013db7fba96c8c72aec7e749ea
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-02 23:41:16 +0200

    service-impl-methods commit

--- src/main/java/com/backend/ireme_ry/_amashuri/service/SchoolServiceImplementation.java
+++ src/main/java/com/backend/ireme_ry/_amashuri/service/SchoolServiceImplementation.java
@@ -1,6 +1,8 @@
 package com.backend.ireme_ry._amashuri.service;
 
+import com.backend.ireme_ry._amashuri.model.District;
 import com.backend.ireme_ry._amashuri.model.School;
+import com.backend.ireme_ry._amashuri.model.User;
 import com.backend.ireme_ry._amashuri.repository.DistrictRepository;
 import com.backend.ireme_ry._amashuri.repository.SchoolRepository;
 import com.backend.ireme_ry._amashuri.repository.UserRepository;
@@ -24,16 +26,19 @@ public class SchoolServiceImplementation implements SchoolService{
 
     @Override
     public List<School> getAllSchools() {
-        return null;
+        return schoolRepository.findAll();
     }
 
     @Override
     public School getSchoolById(Long id) {
-        return null;
+        return schoolRepository.findSchoolById(id);
     }
 
     @Override
     public School saveSchool(School school, Long districtId, Long headteacherId) {
+        District district = districtRepository.findDistrictById(districtId);
+        User user = userRepository.findUserById(headteacherId);
+        
         return null;
     }
 
@@ -44,6 +49,6 @@ public class SchoolServiceImplementation implements SchoolService{
 
     @Override
     public void deleteSchoolById(Long id) {
-
+        schoolRepository.deleteById(id);
     }
 }
