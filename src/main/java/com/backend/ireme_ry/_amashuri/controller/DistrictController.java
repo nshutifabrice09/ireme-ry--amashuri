6e8232b93f9739fe102d281bfc6b90f6d43213e3
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-02 23:51:10 +0200

    initial-controller

--- src/main/java/com/backend/ireme_ry/_amashuri/controller/DistrictController.java
+++ src/main/java/com/backend/ireme_ry/_amashuri/controller/DistrictController.java
@@ -1,2 +1,18 @@
-package com.backend.ireme_ry._amashuri.controller;public class DistrictController {
+package com.backend.ireme_ry._amashuri.controller;
+
+import com.backend.ireme_ry._amashuri.service.DistrictService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+public class DistrictController {
+
+    private final DistrictService districtService;
+
+    @Autowired
+    public DistrictController(DistrictService districtService) {
+        this.districtService = districtService;
+    }
 }
