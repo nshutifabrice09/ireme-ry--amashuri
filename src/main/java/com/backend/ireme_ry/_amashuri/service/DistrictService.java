b6b8da0f6331aa99cece71788306f2832ad54e4f
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-02 22:24:36 +0200

    service-impl commit

--- src/main/java/com/backend/ireme_ry/_amashuri/service/DistrictService.java
+++ src/main/java/com/backend/ireme_ry/_amashuri/service/DistrictService.java
@@ -1,4 +1,14 @@
 package com.backend.ireme_ry._amashuri.service;
 
+import com.backend.ireme_ry._amashuri.model.District;
+
+import java.util.List;
+
 public interface DistrictService {
+    List<District> getAllDistricts();
+    District getDistrictById(Long id);
+    District saveDistrict(District district);
+    District updateDistrict(Long id, District district);
+
+    void deleteById(Long id);
 }
