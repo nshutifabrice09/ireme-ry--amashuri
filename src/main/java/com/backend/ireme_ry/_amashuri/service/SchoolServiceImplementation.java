a533c4157be547e8ec2b03dad22fcb256ef1d8ae
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-02 23:15:53 +0200

    service-impl-methods commit

--- src/main/java/com/backend/ireme_ry/_amashuri/service/SchoolServiceImplementation.java
+++ src/main/java/com/backend/ireme_ry/_amashuri/service/SchoolServiceImplementation.java
@@ -1,12 +1,27 @@
 package com.backend.ireme_ry._amashuri.service;
 
 import com.backend.ireme_ry._amashuri.model.School;
+import com.backend.ireme_ry._amashuri.repository.DistrictRepository;
+import com.backend.ireme_ry._amashuri.repository.SchoolRepository;
+import com.backend.ireme_ry._amashuri.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class SchoolServiceImplementation implements SchoolService{
+    private final SchoolRepository schoolRepository;
+    private final DistrictRepository districtRepository;
+    private final UserRepository userRepository;
+
+    @Autowired
+    public SchoolServiceImplementation(SchoolRepository schoolRepository, DistrictRepository districtRepository, UserRepository userRepository) {
+        this.schoolRepository = schoolRepository;
+        this.districtRepository = districtRepository;
+        this.userRepository = userRepository;
+    }
+
     @Override
     public List<School> getAllSchools() {
         return null;
