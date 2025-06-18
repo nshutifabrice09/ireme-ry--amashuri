97866eb4c4843caa9ec7790a1db731df41421c1d
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-18 18:22:32 +0200

    latest-service-impl-update

--- src/main/java/com/backend/ireme_ry/_amashuri/service/AuthenticationReportServiceImplementation.java
+++ src/main/java/com/backend/ireme_ry/_amashuri/service/AuthenticationReportServiceImplementation.java
@@ -1,12 +1,25 @@
 package com.backend.ireme_ry._amashuri.service;
 
 import com.backend.ireme_ry._amashuri.model.AuthenticationReport;
+import com.backend.ireme_ry._amashuri.repository.AuthenticationReportRepository;
+import com.backend.ireme_ry._amashuri.repository.SchoolRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class AuthenticationReportServiceImplementation implements AuthenticationReportService{
+
+    private final AuthenticationReportRepository authenticationReportRepository;
+    private final SchoolRepository schoolRepository;
+
+    @Autowired
+    public AuthenticationReportServiceImplementation(AuthenticationReportRepository authenticationReportRepository, SchoolRepository schoolRepository) {
+        this.authenticationReportRepository = authenticationReportRepository;
+        this.schoolRepository = schoolRepository;
+    }
+
     @Override
     public List<AuthenticationReport> getAuthenticationReports() {
         return null;
