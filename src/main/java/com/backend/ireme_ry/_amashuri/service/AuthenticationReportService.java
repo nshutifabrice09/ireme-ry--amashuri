65e7eb16f2abc8ffe7dfa9881f54ce26c7093bbd
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-18 18:03:12 +0200

    latest-service-impl-update

--- src/main/java/com/backend/ireme_ry/_amashuri/service/AuthenticationReportService.java
+++ src/main/java/com/backend/ireme_ry/_amashuri/service/AuthenticationReportService.java
@@ -1,4 +1,13 @@
 package com.backend.ireme_ry._amashuri.service;
 
+import com.backend.ireme_ry._amashuri.model.AuthenticationReport;
+
+import java.util.List;
+
 public interface AuthenticationReportService {
+    List<AuthenticationReport> getAuthenticationReports();
+    AuthenticationReport getAuthenticationReportById(Long id);
+    AuthenticationReport saveAuthenticationReport(AuthenticationReport authenticationReport, Long schoolId);
+    AuthenticationReport updateAuthenticationReport(Long id, AuthenticationReport authenticationReport);
+    void removeAuthenticationReport(Long id);
 }
--- src/main/java/com/backend/ireme_ry/_amashuri/service/AuthenticationReportServiceImplementation.java
+++ src/main/java/com/backend/ireme_ry/_amashuri/service/AuthenticationReportServiceImplementation.java
@@ -1,2 +1,34 @@
-package com.backend.ireme_ry._amashuri.service;public class AuthenticationReportServiceImplementation {
+package com.backend.ireme_ry._amashuri.service;
+
+import com.backend.ireme_ry._amashuri.model.AuthenticationReport;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AuthenticationReportServiceImplementation implements AuthenticationReportService{
+    @Override
+    public List<AuthenticationReport> getAuthenticationReports() {
+        return null;
+    }
+
+    @Override
+    public AuthenticationReport getAuthenticationReportById(Long id) {
+        return null;
+    }
+
+    @Override
+    public AuthenticationReport saveAuthenticationReport(AuthenticationReport authenticationReport, Long schoolId) {
+        return null;
+    }
+
+    @Override
+    public AuthenticationReport updateAuthenticationReport(Long id, AuthenticationReport authenticationReport) {
+        return null;
+    }
+
+    @Override
+    public void removeAuthenticationReport(Long id) {
+
+    }
 }
